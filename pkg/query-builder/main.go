faea7b7a3d67672d420fd73b9d7d397dd8d871b2
KwonSoo Park <72953899+Park-KwonSoo@users.noreply.github.com>
2022-01-19 15:38:08 +0900

    feat. query builder update

--- pkg/query-builder/main.go
+++ pkg/query-builder/main.go
@@ -6,97 +6,163 @@ import (
 	"strings"
 )
 
-type query string
+const (
+	SELECT       = "SELECT"
+	INSERT       = "INSERT INTO"
+	CREATE_TABLE = "CREATE TABLE IF NOT EXISTS"
+	ALTER_TABLE  = "ALTER TABLE"
+
+	FROM = "FROM"
+
+	WHERE = "WHERE"
+	VALUE = "VALUES"
+)
+
+var SCHEMA string = "dev"
+
+type query struct {
+	QUERY_TYPE       string //select | insert | create
+	QUERY_TYPE_VALUE string //어떤 것을 select 할지, insert 할지 등
+
+	QUERY_TABLE       string //테이블 이름
+	QUERY_TABLE_VALUE string //테이블 값
+
+	QUERY_WHERE        string //조건절
+	QUERY_WHERE_OPTION string //조건 옵션
+	QUERY_WHERE_VALUE  string //조건 값
+}
 
 //argument를 받아서 새로운 table을 생성한다.
-func CreateTable(t string) query {
-	var b bytes.Buffer
-	b.WriteString("CREATE TABLE IF NOT EXISTS ")
-	b.WriteString(t)
-	b.WriteString(" ")
+func CreateTable(t string) *query {
 
-	s := query(b.String())
-	return s
+	var b bytes.Buffer
+	b.WriteString(SCHEMA)
+	b.WriteString(".")
+	b.WriteString(t)
+
+	q := &query{
+		QUERY_TYPE:         CREATE_TABLE,
+		QUERY_TYPE_VALUE:   "",
+		QUERY_TABLE:        b.String(),
+		QUERY_TABLE_VALUE:  "",
+		QUERY_WHERE:        "",
+		QUERY_WHERE_OPTION: "",
+		QUERY_WHERE_VALUE:  "",
+	}
+
+	return q
 }
 
 //table의 column 정보로 쿼리 생성
-func (q query) TableOption(opt []string) query {
-	var b bytes.Buffer
-	b.WriteString(string(q))
-	b.WriteString("(")
-	b.WriteString(strings.Join(opt, ", "))
-	b.WriteString(")")
+func (q *query) TableComlumn(opt []string) *query {
 
-	s := query(b.String())
-	return s
+	q.QUERY_TABLE_VALUE = fmt.Sprintf("(%s)", strings.Join(opt, ", "))
+
+	return q
 }
 
 //Select할 column들 argument로 받는다.
-func Select(c string) query {
-	var b bytes.Buffer
-	b.WriteString("SELECT ")
-	b.WriteString(c)
-	b.WriteString(" ")
+func Select(c string) *query {
 
-	s := query(b.String())
-	return s
+	q := &query{
+		QUERY_TYPE:         SELECT,
+		QUERY_TYPE_VALUE:   c,
+		QUERY_TABLE:        "",
+		QUERY_TABLE_VALUE:  "",
+		QUERY_WHERE:        "",
+		QUERY_WHERE_OPTION: "",
+		QUERY_WHERE_VALUE:  "",
+	}
+
+	return q
 }
 
 //Table을 argument로 받는다.
-func (q query) From(t string) query {
-	var b bytes.Buffer
-	b.WriteString(string(q))
-	b.WriteString("FROM ")
-	b.WriteString(t)
-	b.WriteString(" ")
+func (q *query) From(t string) *query {
 
-	s := query(b.String())
-	return s
+	q.QUERY_TABLE = FROM
+
+	var b bytes.Buffer
+	b.WriteString(SCHEMA)
+	b.WriteString(".")
+	b.WriteString(t)
+
+	q.QUERY_TABLE_VALUE = b.String()
+
+	return q
+
 }
 
 //where절의 column과 value를 argument로 받는다
-func (q query) Where(c string, v interface{}) query {
-	var b bytes.Buffer
-	b.WriteString(string(q))
-	b.WriteString("WHERE ")
-	b.WriteString(c)
-	b.WriteString("=")
-	b.WriteString("'")
-	b.WriteString(fmt.Sprint(v))
-	b.WriteString("'")
+func (q *query) Where(c string, v interface{}) *query {
 
-	s := query(b.String())
-	return s
+	q.QUERY_WHERE = WHERE
+	q.QUERY_WHERE_OPTION = c
+	q.QUERY_WHERE_VALUE = fmt.Sprintf("='%v'", v)
+
+	return q
 }
 
 //argument로 table과 colume을 받는다.
-func Insert(t string, c string) query {
+func Insert(t string, c string) *query {
+
+	var table bytes.Buffer
+	table.WriteString(SCHEMA)
+	table.WriteString(".")
+	table.WriteString(t)
+
 	var b bytes.Buffer
-	b.WriteString("INSERT INTO ")
-	b.WriteString(t)
-	b.WriteString(" ")
 	b.WriteString("(")
 	b.WriteString(c)
-	b.WriteString(") ")
-
-	s := query(b.String())
-	return s
-}
-
-func (q query) Value(v []string) query {
-	var b bytes.Buffer
-	b.WriteString(string(q))
-	b.WriteString("VALUE (")
-
-	value := strings.Join(v, ", ")
-	b.WriteString(value)
 	b.WriteString(")")
 
-	s := query(b.String())
-	return s
+	q := &query{
+		QUERY_TYPE:         INSERT,
+		QUERY_TYPE_VALUE:   "",
+		QUERY_TABLE:        table.String(),
+		QUERY_TABLE_VALUE:  b.String(),
+		QUERY_WHERE:        "",
+		QUERY_WHERE_OPTION: "",
+		QUERY_WHERE_VALUE:  "",
+	}
+
+	return q
+}
+
+//추가할 데이터의 column에 대한 value 값을 받는다.
+func (q *query) Value(v []string) *query {
+
+	q.QUERY_WHERE = VALUE
+
+	var b bytes.Buffer
+	b.WriteString("(")
+	b.WriteString(strings.Join(v, ", "))
+	b.WriteString(")")
+
+	q.QUERY_WHERE_VALUE = b.String()
+
+	return q
 }
 
 //마지막으로 string으로 변환하는 receiver
 func (q query) ToString() string {
-	return string(q)
+	var b bytes.Buffer
+	b.WriteString(q.QUERY_TYPE)
+	b.WriteString(" ")
+	b.WriteString(q.QUERY_TYPE_VALUE)
+	b.WriteString(" ")
+
+	b.WriteString(q.QUERY_TABLE)
+	b.WriteString(" ")
+	b.WriteString(q.QUERY_TABLE_VALUE)
+	b.WriteString(" ")
+
+	b.WriteString(q.QUERY_WHERE)
+	b.WriteString(" ")
+	b.WriteString(q.QUERY_WHERE_OPTION)
+	b.WriteString(" ")
+	b.WriteString(q.QUERY_WHERE_VALUE)
+	b.WriteString(" ")
+
+	return b.String()
 }
